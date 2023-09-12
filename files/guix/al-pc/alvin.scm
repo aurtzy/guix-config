dc6c52956c565df8f07078a20f89f13306e90478
aurtzy <aurtzy@gmail.com>
2023-09-12 11:56:59 -0400

    update al-pc configurations

--- files/guix/al-pc/alvin.scm
+++ files/guix/al-pc/alvin.scm
@@ -2,11 +2,11 @@
              (gnu services)
              (my-guix extensions)
              (my-guix home base foreign-desktop)
+             (my-guix home extensions channels)
              (my-guix home extensions common)
              (my-guix home extensions desktop-environment)
              (my-guix home extensions entertainment)
-             (my-guix home extensions extras)
-             (my-guix home extensions foreign)
+             (my-guix home extensions extra)
              (my-guix home extensions hardware)
              (my-guix home extensions server)
              (my-guix home services)
@@ -34,9 +34,10 @@
                                "library"
                                "attic")))
             (home-environment-user-services env)))))
- (append (list plasma-extension
+ (append common-extensions
+         extra-extensions
+         entertainment-extensions
+         (list plasma-extension
                pipewire-extension
-               web-server-extension)
-         common-extensions
-         extras-extensions
-         entertainment-extensions))
+               web-server-extension
+               nonguix-channel-extension)))
