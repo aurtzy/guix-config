25bc546ad88152d89eed8c1831a35d313c2fc7bc
aurtzy <aurtzy@gmail.com>
2023-09-24 04:46:10 -0400

    update python3 manifest

--- files/manifests/python3.scm
+++ files/manifests/python3.scm
@@ -1,6 +1,7 @@
 (use-modules (gnu))
 
-(use-package-modules python python-xyz)
+(use-package-modules python python-xyz emacs-xyz)
 
 (packages->manifest (list python-lsp-server
-                          python-isort))
+                          python-isort
+                          emacs-pyvenv))
