e751aa1d80d7808b3cce9ed0b840e413a750780a
aurtzy <aurtzy@gmail.com>
2023-10-29 05:01:24 -0400

    Update git-annex-configure
    It seems to me now that having a local copy of the repository is redundant and
    doesn't actually have any benefits, so it seemed reasonable to remove it and
    rewrite the package to use the repository stored on GitHub.

--- modules/my-guix/packages/git-annex-configure.scm
+++ modules/my-guix/packages/git-annex-configure.scm
@@ -19,8 +19,8 @@
   #:use-module (guix gexp)
   #:use-module (guix packages)
   #:use-module ((guix licenses) #:prefix license:)
-  #:use-module (guix download)
-  #:use-module (guix build-system gnu)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system guile)
   #:use-module (gnu packages)
   #:use-module (gnu packages autotools)
   #:use-module (gnu packages guile)
@@ -28,8 +28,51 @@
   #:use-module (gnu packages haskell-apps)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages texinfo)
-  #:use-module (my-guix utils))
+  #:use-module (gnu packages version-control))
 
 (define-public git-annex-configure
-  ;; Use local copy of git-annex-configure
-  (primitive-load (path-append-my-files "git-annex-configure/guix.scm")))
+  (let ((commit "cadba76339ca3bb6f3844e3402dcea1e63fabe6e")
+        (revision "1"))
+    (package
+      (name "git-annex-configure")
+      (version (git-version "1.0.0" revision commit))
+      (source (origin
+                (uri
+                 (git-reference
+                  (url "https://github.com/aurtzy/git-annex-configure.git")
+                  (commit commit)))
+                (method git-fetch)
+                (sha256
+                 (base32
+                  "1cjhzmdv9j6dabh3w5yjhd80x9anlnadyfl594jcgmmzj7p4pfgz"))
+                (file-name (git-file-name name version))))
+      (build-system guile-build-system)
+      (arguments
+       (list
+        #:source-directory "src"
+        #:phases
+        #~(modify-phases %standard-phases
+            (add-after 'install 'install-scripts
+              (lambda* (#:key outputs #:allow-other-keys)
+                (let ((out (assoc-ref outputs "out")))
+                  (mkdir (string-append out "/bin"))
+                  (symlink #$(program-file
+                              "git-annex-configure"
+                              '(begin
+                                 (use-modules (git-annex-configure main))
+                                 (main (command-line))))
+                           (string-append out
+                                          "/bin/git-annex-configure"))))))))
+      (native-inputs
+       ;; Specify guile-3.0 here for guile-build-system and also in
+       ;; propagated-inputs for runtime features (git hook scripts)
+       (list guile-3.0))
+      (propagated-inputs
+       (list guile-3.0
+             git
+             git-annex))
+      (synopsis "Declarative git-annex configuration with Guile Scheme")
+      (description
+       "git-annex-configure is a git-annex addon command for reducing the maintenance burden of applying repository settings.  More specifically, git-annex-configure lets you specify settings for your repositories in Guile and can apply most of your configurations with a single execution of the command.")
+      (home-page "https://github.com/aurtzy/git-annex-configure")
+      (license license:gpl3+))))
