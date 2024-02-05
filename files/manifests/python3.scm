dc9e05e0506ca910821d95e9893483ec2e4cbfba
aurtzy <aurtzy@gmail.com>
2024-02-05 02:12:48 -0500

    files: Revert modification to python3 manifest
    Since it looks like it'll be difficult to selectively apply mode maps in
    Emacs - i.e. `python-mode' to `python-ts-mode' - as they both belong in the same
    package (`python.el'), this removes `tree-sitter-python' from the manifest in
    favor of declaring it all in an Emacs mod.

--- files/manifests/python3.scm
+++ files/manifests/python3.scm
@@ -1,9 +1,8 @@
 (use-modules (gnu))
 
-(use-package-modules python-xyz tree-sitter)
+(use-package-modules python-xyz)
 
 (packages->manifest (list python-lsp-server
-                          tree-sitter-python
                           ;; Below are packages that have to be installed in
                           ;; Python environment if using `layout' with direnv
                           python-isort))
