411fde1de34b2a2d20ed59960e21f8b6d9e6cdc9
aurtzy <aurtzy@gmail.com>
2024-02-05 01:39:56 -0500

    files: Add comment to python3 manifest
    Adds a note about using python-isort or any other editor-utilized Python
    packages that may be added, since direnv's `layout python3' will (as expected)
    not allow system packages (i.e. Guix environment in this case) to leak into
    development environment.

--- files/manifests/python3.scm
+++ files/manifests/python3.scm
@@ -3,5 +3,7 @@
 (use-package-modules python-xyz tree-sitter)
 
 (packages->manifest (list python-lsp-server
-                          python-isort
-                          tree-sitter-python))
+                          tree-sitter-python
+                          ;; Below are packages that have to be installed in
+                          ;; Python environment if using `layout' with direnv
+                          python-isort))
