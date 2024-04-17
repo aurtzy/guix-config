924ee216203292f6977ccbea4a2314ba34b4c4b6
aurtzy <aurtzy@gmail.com>
2024-04-17 03:23:40 -0400

    my-guix: libdecor: Add space to description for consistency

--- modules/my-guix/packages/xorg.scm
+++ modules/my-guix/packages/xorg.scm
@@ -63,6 +63,6 @@
     (home-page "https://gitlab.freedesktop.org/libdecor/libdecor")
     (synopsis "Client-side decorations library for Wayland clients")
     (description "libdecor is a library that can help Wayland clients draw
-window decorations for them. It aims to provide multiple backends that
+window decorations for them.  It aims to provide multiple backends that
 implements the decoration drawing.")
     (license license:expat)))
