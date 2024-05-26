fb22d9792784478f932b771d63264596cfeff5f6
aurtzy <aurtzy@gmail.com>
2024-05-25 23:51:02 -0400

    files/guix: al-pc: Use modded-system for system-level configuration

--- files/guix/al-pc.scm
+++ files/guix/al-pc.scm
@@ -55,97 +55,103 @@
                      "1yirhgjaipja17jd0mzf0zcajghzpln6a9qmiwq6glz69qf5hm1l")))))
       (version (git-version "gfxstrand-nvk" revision commit)))))
 
+(define initial-operating-system
+  (let ((base-os base-desktop-operating-system))
+    (operating-system
+      (inherit base-os)
+      (host-name "al-pc")
+      (kernel linux-gfxstrand-nvk)
+      (initrd microcode-initrd)
+      (firmware (list linux-firmware))
+      (kernel-arguments
+       (cons*
+        ;; vfio
+        ;; "intel_iommu=on"
+        ;; "iommu=pt"
+        (operating-system-user-kernel-arguments base-os)))
+      (users
+       (cons* (user-account
+               (name "alvin")
+               (comment "Alvin")
+               (group "users")
+               (home-directory "/home/alvin")
+               (supplementary-groups '("wheel"
+                                       "netdev"
+                                       "audio"
+                                       "video"
+                                       "kvm"
+                                       "libvirt")))
+              (operating-system-users base-os)))
+      (mapped-devices
+       (list (mapped-device
+              (source
+               (uuid "7ccff0a3-b181-4788-9892-e68306566325"))
+              (target "cryptroot")
+              ;; TODO Get key file working with root drive
+              (type luks-device-mapping))
+             (mapped-device
+              (source
+               (uuid "d50f62c1-e312-4c83-8b55-b0af00a4de2a"))
+              (target "cryptstorage")
+              (type (luks-device-mapping-with-options
+                     #:key-file (string-append "/root/keys/"
+                                               (uuid->string source)))))))
+      (file-systems
+       (cons* (file-system
+                (mount-point "/")
+                (device "/dev/mapper/cryptroot")
+                (flags
+                 (base-file-system-flags-ref 'btrfs 'ssd))
+                (options
+                 (alist->file-system-options
+                  (base-file-system-options-ref 'btrfs 'ssd)))
+                (type "btrfs")
+                (dependencies (filter
+                               (lambda (dev)
+                                 (member "cryptroot"
+                                         (mapped-device-targets dev)))
+                               mapped-devices)))
+              (file-system
+                (mount-point "/home/alvin/storage")
+                (device "/dev/mapper/cryptstorage")
+                (flags
+                 (base-file-system-flags-ref 'btrfs 'hdd))
+                (options
+                 (alist->file-system-options
+                  (base-file-system-options-ref 'btrfs 'hdd)))
+                (type "btrfs")
+                (create-mount-point? #t)
+                (mount-may-fail? #t)
+                (dependencies mapped-devices))
+              (file-system
+                (mount-point "/boot/efi")
+                (device (uuid "DC21-DB63" 'fat32))
+                (type "vfat"))
+              (file-system
+                (mount-point "/media/backup")
+                (device "/dev/mapper/luks-f42810d8-c723-4521-9646-da12f6103b59")
+                (flags
+                 (base-file-system-flags-ref 'btrfs 'hdd))
+                (options
+                 (alist->file-system-options
+                  '(("compress-force" . "zstd:10"))))
+                (type "btrfs")
+                (mount? #f))
+              (operating-system-file-systems base-os)))
+      (services
+       (cons* (service keyboard-center-service-type)
+              (operating-system-user-services base-os))))))
+
+(define system
+  (modded-system
+    (mods (append desktop-mods
+                  (list gnome-mod
+                        nvidia-mod)))
+    (initial-os initial-operating-system)))
+
 (parameterize ((swapfile (swapfile-configuration
                           (file "/swapfile")
                           (device "/dev/mapper/cryptroot")
                           (offset "5250304")))
                (replace-mesa replace-mesa->mesa-nvk-git))
-  (apply-mods
-   (let ((base-os base-desktop-operating-system))
-     (operating-system
-       (inherit base-os)
-       (host-name "al-pc")
-       (kernel linux-gfxstrand-nvk)
-       (initrd microcode-initrd)
-       (firmware (list linux-firmware))
-       (kernel-arguments
-        (cons*
-         ;; vfio
-         ;; "intel_iommu=on"
-         ;; "iommu=pt"
-         (operating-system-user-kernel-arguments base-os)))
-       (users
-        (cons* (user-account
-                (name "alvin")
-                (comment "Alvin")
-                (group "users")
-                (home-directory "/home/alvin")
-                (supplementary-groups '("wheel"
-                                        "netdev"
-                                        "audio"
-                                        "video"
-                                        "kvm"
-                                        "libvirt")))
-               %base-user-accounts))
-       (mapped-devices
-        (list (mapped-device
-               (source
-                (uuid "7ccff0a3-b181-4788-9892-e68306566325"))
-               (target "cryptroot")
-               ;; TODO Get key file working with root drive
-               (type luks-device-mapping))
-              (mapped-device
-               (source
-                (uuid "d50f62c1-e312-4c83-8b55-b0af00a4de2a"))
-               (target "cryptstorage")
-               (type (luks-device-mapping-with-options
-                      #:key-file (string-append "/root/keys/"
-                                                (uuid->string source)))))))
-       (file-systems
-        (cons* (file-system
-                 (mount-point "/")
-                 (device "/dev/mapper/cryptroot")
-                 (flags
-                  (base-file-system-flags-ref 'btrfs 'ssd))
-                 (options
-                  (alist->file-system-options
-                   (base-file-system-options-ref 'btrfs 'ssd)))
-                 (type "btrfs")
-                 (dependencies (filter
-                                (lambda (dev)
-                                  (member "cryptroot"
-                                          (mapped-device-targets dev)))
-                                mapped-devices)))
-               (file-system
-                 (mount-point "/home/alvin/storage")
-                 (device "/dev/mapper/cryptstorage")
-                 (flags
-                  (base-file-system-flags-ref 'btrfs 'hdd))
-                 (options
-                  (alist->file-system-options
-                   (base-file-system-options-ref 'btrfs 'hdd)))
-                 (type "btrfs")
-                 (create-mount-point? #t)
-                 (mount-may-fail? #t)
-                 (dependencies mapped-devices))
-               (file-system
-                 (mount-point "/boot/efi")
-                 (device (uuid "DC21-DB63" 'fat32))
-                 (type "vfat"))
-               (file-system
-                 (mount-point "/media/backup")
-                 (device "/dev/mapper/luks-f42810d8-c723-4521-9646-da12f6103b59")
-                 (flags
-                  (base-file-system-flags-ref 'btrfs 'hdd))
-                 (options
-                  (alist->file-system-options
-                   '(("compress-force" . "zstd:10"))))
-                 (type "btrfs")
-                 (mount? #f))
-               (operating-system-file-systems base-os)))
-       (services
-        (cons* (service keyboard-center-service-type)
-               (operating-system-user-services base-os)))))
-   (append desktop-mods
-           (list gnome-mod
-                 nvidia-mod))))
+  (modded-system->operating-system system))
