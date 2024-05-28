7d0609db3a0ce9a433894be0dbc5f5c3c70f04e8
aurtzy <aurtzy@gmail.com>
2024-05-28 10:46:37 -0400

    my-guix: Merge desktop-environment mods

--- modules/my-guix/mods/desktop-environment.scm
+++ modules/my-guix/mods/desktop-environment.scm
@@ -21,13 +21,20 @@
 
 (define-module (my-guix mods desktop-environment)
   #:use-module (gnu)
+  #:use-module (gnu home)
+  #:use-module (gnu home services)
+  #:use-module (gnu home services shells)
+  #:use-module (gnu services)
+  #:use-module (guix gexp)
   #:use-module (my-guix mods)
   #:use-module (my-guix mods desktop)
   #:use-module (my-guix mods hardware)
   #:use-module (my-guix utils)
-  #:export (gnome-mod))
+  #:export (gnome-mod
+            plasma-mod
+            wayland-mod))
 
-(use-package-modules freedesktop gnome gnome-xyz)
+(use-package-modules freedesktop gnome gnome-xyz qt)
 
 (use-service-modules desktop xorg)
 
@@ -36,6 +43,8 @@
     (name 'gnome)
     (description
      "Provides configurations for the GNOME desktop environment.")
+    (dependencies
+     (list wayland-mod))
     (os-extension
      (compose-lambda (os)
        (let ((replace-mesa (replace-mesa))
@@ -72,3 +81,55 @@
                            (extra-packages
                             (list (replace-mesa gnome-essential-extras)))))
                  (service gdm-service-type)))))))))
+
+(define plasma-mod-shortcuts
+  #~(begin
+      ;; Use Overview as default action for Meta
+      ;; See: https://zren.github.io/kde/#windowsmeta-key
+      (invoke "kwriteconfig5"
+              "--file"
+              (string-append (getenv "HOME")
+                             "/.config/kwinrc")
+              "--group"
+              "ModifierOnlyShortcuts"
+              "--key"
+              "Meta"
+              (string-join
+               (list "org.kde.kglobalaccel"
+                     "/component/kwin"
+                     "org.kde.kglobalaccel.Component"
+                     "invokeShortcut"
+                     "Overview")
+               ","))
+      (invoke "qdbus"
+              "org.kde.KWin"
+              "/KWin"
+              "reconfigure")))
+
+(define plasma-mod
+  (mod
+    (name 'plasma)
+    (description
+     "Configures the KDE Plasma desktop environment for this system.")
+    (dependencies
+     (list wayland-mod))
+    (he-extension
+     (compose
+      (mod-he-services
+       (list (simple-service name
+                             home-activation-service-type
+                             plasma-mod-shortcuts)))))))
+
+(define wayland-mod
+  (mod
+    (name 'wayland)
+    (description
+     "Configures environment for usage on Wayland compositors.")
+    (he-extension
+     (compose
+      (mod-he-services
+       (list (simple-service name
+                             home-bash-service-type
+                             (home-bash-extension
+                              (environment-variables
+                               '(("MOZ_ENABLE_WAYLAND" . "1")))))))))))
