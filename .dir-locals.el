f946f1535f799ebd43a310c143c600d919399400
aurtzy <aurtzy@gmail.com>
2024-06-10 13:08:29 -0400

    dir-locals.el: Exclude some directories from magit-todos

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,9 +1,10 @@
-;;; Directory Local Variables         -*- no-byte-compile: t; -*-
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((scheme-mode
-  .
-  ((eval . (guix-devel-mode t))
-   (eval . (put 'compose-lambda 'scheme-indent-function 1))
-   (eval . (put 'mod 'scheme-indent-function 0))
-   (eval . (put 'modded-system 'scheme-indent-function 0)))))
+((magit-status-mode . ((magit-todos-exclude-globs . (".git/"
+                                                     ".direnv/"
+                                                     "/files/patches"))))
+ (scheme-mode . ((eval . (guix-devel-mode t))
+                 (eval . (put 'compose-lambda 'scheme-indent-function 1))
+                 (eval . (put 'mod 'scheme-indent-function 0))
+                 (eval . (put 'modded-system 'scheme-indent-function 0)))))
