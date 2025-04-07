fa10c4ee8c76aadea66b8d846c95b89811c5e1d5
aurtzy <aurtzy@gmail.com>
2025-04-06 23:58:21 -0400

    dir-locals: Set `disproject-custom-suffixes`

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,55 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((magit-status-mode . ((magit-todos-exclude-globs . (".git/"
+((nil
+  .
+  ((disproject-custom-suffixes
+    .
+    (["Guix options"
+      ("-a" "Allow downgrades" "--allow-downgrades")
+      ("-da" "Disable authentication (pull)" "--disable-authentication")
+      ("-df" "Disable flatpak" "--disable-flatpak")]
+     ["Run guix..."
+      :pad-keys t
+      ("h r" "home reconfigure" disproject-compile
+       :cmd (lambda (args)
+              (interactive (list (transient-args transient-current-command)))
+              (let ((allow-downgrades?
+                     (transient-arg-value "--allow-downgrades" args))
+                    (disable-flatpak?
+                     (transient-arg-value "--disable-flatpak" args)))
+                (concat
+                 (if disable-flatpak? "GUIX_FLATPAK_DISABLE=1 " "")
+                 "guix home reconfigure config.scm"
+                 (if allow-downgrades? " --allow-downgrades" ""))))
+       :buffer-id "guix-home"
+       :comint? t)
+      ("p" "pull" disproject-compile
+       :cmd (lambda (args)
+              (interactive (list (transient-args transient-current-command)))
+              (let ((allow-downgrades?
+                     (transient-arg-value "--allow-downgrades" args))
+                    (disable-authentication?
+                     (transient-arg-value "--disable-authentication" args)))
+                (concat
+                 "guix pull"
+                 (if allow-downgrades? " --allow-downgrades" "")
+                 (if disable-authentication?
+                     " --disable-authentication"
+                   ""))))
+       :buffer-id "guix-pull"
+       :comint? t)
+      ("s r" "system reconfigure" disproject-compile
+       :cmd (lambda (args)
+              (interactive (list (transient-args transient-current-command)))
+              (let ((allow-downgrades?
+                     (transient-arg-value "--allow-downgrades" args)))
+                (concat
+                 "sudo guix system reconfigure config.scm"
+                 (if allow-downgrades? " --allow-downgrades" ""))))
+       :buffer-id "guix-system"
+       :comint? t)]))))
+ (magit-status-mode . ((magit-todos-exclude-globs . (".git/"
                                                      ".direnv/"
                                                      "/files/patches"))))
  (scheme-mode . ((eval . (guix-devel-mode t))
